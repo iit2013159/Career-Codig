14f2ea5e4e17eb4afed8735dffd8a5aba1f311be
Abhishek <iit2013159@iiita.ac.in>
2018-07-04 00:46:20 +0530

    Add files via upload

--- final.bat
+++ final.bat
@@ -1,7 +1,47 @@
 @echo off
 SETLOCAL EnableDelayedExpansion
 set nameindex=0
-set chathtml=<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>new Sample</title><script>
+set "chathtml=<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>chat History</title><script>"
+set "line1=function myFunction(idname) {"
+set "line2=var nameArray =this[idname];"
+set "line3=var finalcontent='';"
+set "line4=for (var ab = 0; ab < nameArray.length ; ab++){"
+set "line5=var date1=nameArray[ab];"
+set "line6=var content ="^<div id='"+date1+"_dates"+"' style=''^>";"
+set "line7=    var datename = idname.concat("_").concat(date1);"
+set "line8=content +="^<button id='"+datename+"' value='"+datename+"' onclick='showHtml(this.id)'^>"+date1+"^</button^>^<br /^>\n" ;"
+set "line9=content +="^<div id='"+datename.concat("_html") + "' style='visibility: hidden'^>";"
+set "line10=content += this[datename];"
+set "line11=content +="^</div^>";"
+set "line12=    finalcontent+=content;"
+set "line13=}"
+set "line14=document.getElementById("left").innerHTML=finalcontent;"
+set "line15=}"
+set "line16=function abc(idname) {"
+set "line17=var div = document.getElementById(idname);"
+set "line18=div.style.display='block';"
+set "line19=div.style.visibility= 'visible';"
+set "line20=}"
+set "line21=function showHtml(idname) {"
+set "line22=alert(idname);"
+set "line23=var div = document.getElementById(idname.concat("_html"));"
+set "line24=div.style.display='block';"
+set "line25=div.style.visibility= 'visible';"
+set "line26=div.style.resize ='both';"
+set "line27=div.style.overflow ='auto';"
+set "line28=}"
+set "line29=function setdiv() {"
+set "line30=var div = document.getElementById("names");"
+set "line31=for(var i = 0; i < commName.length;i++){"
+set "line32=div.innerHTML +="^<button id='"+commName[i]+"' value='"+commName[i]+"' onclick='myFunction(this.id)'^>"+commName[i]+"^</button^>^<br /^>";"
+set "line33=} }"
+set "line34=</script></head>"
+set "line35=<body style="overflow: auto" onload="setdiv() ">"
+set "line36=<div id ="top">"
+set "line37=<div id="names" ></div></div>"
+set "line38=<div id ="left"></div>"
+set "line39=<div id ="bottom"></div>"
+set "line40=</body></html>"
 goto :main
 
 
@@ -142,11 +182,21 @@ for /l %%n in (0,1,!sizeofarray!) do (
 
 goto :eof
 
+:calllines
+set /A total=40
+echo !total!
+for /l %%n in (1,1,!total!) do (
+    echo !line%%n!
+)
+
+goto :eof
 
 :main
 call :readepeoplename
 SET /A nameindex=!nameindex!-2
-call :printarray folderlist !nameindex! >newsample.txt
-
+echo !chathtml! >chathistory.html
+call :printarray folderlist !nameindex! >>chathistory.html
+call :calllines >>chathistory.html
+start "" "chathistory.html"
+rem start chrome.exe "" "newsample.html"
 goto :eof
-
